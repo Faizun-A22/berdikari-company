36567f89751883bc231668fb47cb709aeb83e2e5
Faizun-A22 <faizacket@gmail.com>
2026-07-11 09:27:49 +0700

    perbaiki

--- server/index.js
+++ server/index.js
@@ -58,6 +58,12 @@ const upload = multer({
 
 // Middleware
 app.use(cors());
+app.use('/api', (req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use('/api/uploads', express.static(uploadsDir));
